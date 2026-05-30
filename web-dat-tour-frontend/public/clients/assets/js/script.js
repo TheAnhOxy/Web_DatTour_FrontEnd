c2d9c01100991616b5e40d529a07377f0544aa5b
duchauuuuu <22679541.hau@student.iuh.edu.vn>
2026-05-31 01:22:41 +0700

    fix: fix lỗi animate

--- web-dat-tour-frontend/public/clients/assets/js/script.js
+++ web-dat-tour-frontend/public/clients/assets/js/script.js
@@ -7,8 +7,8 @@
 
     Note: This is Main JS File.
 -----------------------------------------------------------------------------------
-	CSS INDEX
-	===================
+    CSS INDEX
+    ===================
     ## Header Style
     ## Dropdown menu
     ## Submenu
@@ -55,8 +55,8 @@
             }
         }
         headerStyle();
-        
-        
+
+
         // ## Dropdown menu
         var mobileWidth = 992;
         var navcollapse = $('.navigation li.dropdown');
@@ -67,7 +67,7 @@
                 $(this).children('.megamenu').stop(true, false, true).slideToggle(300);
             }
         });
-        
+
         // ## Submenu Dropdown Toggle
         if ($('.main-header .navigation li.dropdown ul').length) {
             $('.main-header .navigation li.dropdown').append('<div class="dropdown-btn"><span class="far fa-angle-down"></span></div>');
@@ -77,59 +77,59 @@
                 $(this).prev('ul').slideToggle(500);
                 $(this).prev('.megamenu').slideToggle(800);
             });
-            
+
             //Disable dropdown parent link
             $('.navigation li.dropdown > a').on('click', function (e) {
                 e.preventDefault();
             });
         }
-        
+
         //Submenu Dropdown Toggle
         if ($('.main-header .main-menu').length) {
             $('.main-header .main-menu .navbar-toggle').click(function () {
                 $(this).prev().prev().next().next().children('li.dropdown').hide();
             });
         }
-        
-         
+
+
         // ## Menu Hidden Sidebar Content Toggle
-        if($('.menu-sidebar').length){
+        if ($('.menu-sidebar').length) {
             //Show Form
             // $('.menu-sidebar').on('click', function(e) {
             //     e.preventDefault();
             //     $('body').toggleClass('side-content-visible');
             // });
             //Hide Form
-            $('.hidden-bar .inner-box .cross-icon,.form-back-drop,.close-menu').on('click', function(e) {
+            $('.hidden-bar .inner-box .cross-icon,.form-back-drop,.close-menu').on('click', function (e) {
                 e.preventDefault();
                 $('body').removeClass('side-content-visible');
             });
             //Dropdown Menu
-            $('.fullscreen-menu .navigation li.dropdown > a').on('click', function() {
+            $('.fullscreen-menu .navigation li.dropdown > a').on('click', function () {
                 $(this).next('ul').slideToggle(500);
             });
         }
-         
-        
+
+
         // ## Search Box
-		$('.nav-search > button').on('click', function () {
-			$('.nav-search form').toggleClass('hide');
-		});
-        
-        
+        $('.nav-search > button').on('click', function () {
+            $('.nav-search form').toggleClass('hide');
+        });
+
+
         // Hide Box Search WHEN CLICK OUTSIDE
-		if ($(window).width() > 767){
-			$('body').on('click', function (event) {
-				if ($('.nav-search > button').has(event.target).length == 0 && !$('.nav-search > button').is(event.target)
-					&& $('.nav-search form').has(event.target).length == 0 && !$('.nav-search form').is(event.target)) {
-					if ($('.nav-search form').hasClass('hide') == false) {
-						$('.nav-search form').toggleClass('hide');
-					};
-				}
-			});
-		}
-        
-        
+        if ($(window).width() > 767) {
+            $('body').on('click', function (event) {
+                if ($('.nav-search > button').has(event.target).length == 0 && !$('.nav-search > button').is(event.target)
+                    && $('.nav-search form').has(event.target).length == 0 && !$('.nav-search form').is(event.target)) {
+                    if ($('.nav-search form').hasClass('hide') == false) {
+                        $('.nav-search form').toggleClass('hide');
+                    };
+                }
+            });
+        }
+
+
         // ## Sidebar Menu
         if ($('.sidebar-menu li.dropdown ul').length) {
             $('.sidebar-menu li.dropdown').append('<div class="dropdown-btn"><span class="far fa-angle-down"></span></div>');
@@ -140,7 +140,7 @@
                 $(this).prev('.megamenu').slideToggle(800);
                 $(this).parent('li.dropdown').toggleClass('active');
             });
-            
+
             //Disable dropdown parent link
             $('.sidebar-menu li.dropdown > a').on('click', function (e) {
                 e.preventDefault();
@@ -148,8 +148,8 @@
                 $(this).parent('li.dropdown').toggleClass('active');
             });
         }
-        
-  
+
+
         // ## Video Popup
         if ($('.video-play').length) {
             $('.video-play').magnificPopup({
@@ -157,7 +157,7 @@
             });
         }
 
-        
+
         // ## Testimonial Slider
         if ($('.testimonials-active').length) {
             $('.testimonials-active').slick({
@@ -173,7 +173,7 @@
             });
         }
 
-        
+
         // ## Destination Carousel
         if ($('.destination-active').length) {
             $('.destination-active').slick({
@@ -215,8 +215,8 @@
                 ]
             });
         }
-        
-        
+
+
         // ## Client Logo Carousel
         if ($('.client-logo-active').length) {
             $('.client-logo-active').slick({
@@ -251,8 +251,8 @@
                 ]
             });
         }
-        
- 
+
+
         // ## Hot Deals Carousel
         if ($('.hot-deals-active').length) {
             $('.hot-deals-active').slick({
@@ -281,7 +281,7 @@
                 ]
             });
         }
-        
+
 
         // ## Gallery Slider
         if ($('.gallery-slider-active').length) {
@@ -318,8 +318,8 @@
                 ]
             });
         }
-        
-        
+
+
         // ## Product Slider
         if ($('.product-slider').length) {
             $('.product-slider').slick({
@@ -355,8 +355,8 @@
                 ]
             });
         }
-        
-        
+
+
         // ## Gallery Popup
         $('.gallery a').magnificPopup({
             type: 'image',
@@ -365,8 +365,8 @@
                 navigateByImgClick: true,
             },
         });
-        
-        
+
+
         // ## Instagram Gallery 
         $('.instagram-item').magnificPopup({
             type: 'image',
@@ -375,8 +375,8 @@
                 navigateByImgClick: true,
             },
         });
-        
-        
+
+
         // ## SkillBar
         if ($('.skillbar').length) {
             $('.skillbar').appear(function () {
@@ -387,12 +387,12 @@
                 });
             });
         }
-        
-        
-         /* ## Fact Counter + Text Count - Our Success */
+
+
+        /* ## Fact Counter + Text Count - Our Success */
         if ($('.counter-text-wrap').length) {
             $('.counter-text-wrap').appear(function () {
-                
+
                 var $t = $(this),
                     n = $t.find(".count-text").attr("data-stop"),
                     r = parseInt($t.find(".count-text").attr("data-speed"), 10);
@@ -419,30 +419,30 @@
                 accY: 0
             });
         }
-        
-        
+
+
         // ## Destinations Filter
         $('.destinations-active').imagesLoaded(function () {
-			var items = $('.destinations-active').isotope({
-				itemSelector: '.item',
-				percentPosition: true,
-			});
-			// items on button click
-			$('.destinations-nav').on('click', 'li', function () {
-				var filterValue = $(this).attr('data-filter');
-				items.isotope({
-					filter: filterValue
-				});
-			});
-			// menu active class
-			$('.destinations-nav li').on('click', function (event) {
-				$(this).siblings('.active').removeClass('active');
-				$(this).addClass('active');
-				event.preventDefault();
-			});
-		});
-        
-        
+            var items = $('.destinations-active').isotope({
+                itemSelector: '.item',
+                percentPosition: true,
+            });
+            // items on button click
+            $('.destinations-nav').on('click', 'li', function () {
+                var filterValue = $(this).attr('data-filter');
+                items.isotope({
+                    filter: filterValue
+                });
+            });
+            // menu active class
+            $('.destinations-nav li').on('click', function (event) {
+                $(this).siblings('.active').removeClass('active');
+                $(this).addClass('active');
+                event.preventDefault();
+            });
+        });
+
+
         // ## Price Range Fliter jQuery UI
         if ($('.price-slider-range').length) {
             $(".price-slider-range").slider({
@@ -456,24 +456,24 @@
                 }
             });
             $("#price").val(
-                $(".price-slider-range").slider("values", 0).toLocaleString('vi-VN') + 
-                " - " + 
+                $(".price-slider-range").slider("values", 0).toLocaleString('vi-VN') +
+                " - " +
                 $(".price-slider-range").slider("values", 1).toLocaleString('vi-VN') + " vnđ"
             );
         }
-        
-        
-        
+
+
+
         // ## Hover Content
         $('.hover-content').hover(
-            function(){
+            function () {
                 $(this).find('.inner-content').slideDown();
-            }, function() {
+            }, function () {
                 $(this).find('.inner-content').slideUp();
             }
         );
-        
-        
+
+
         // ## Scroll to Top
         if ($('.scroll-to-target').length) {
             $(".scroll-to-target").on('click', function () {
@@ -485,19 +485,19 @@
 
             });
         }
-        
-        
+
+
         // ## Nice Select
         $('select').niceSelect();
-        
-        
+
+
         // ## AOS Animation
         AOS.init();
-        
- 
+
+
     });
-    
-    
+
+
     /* ==========================================================================
        When document is resize, do
     ========================================================================== */
@@ -547,7 +547,7 @@
             }
         }
         handlePreloader();
-        
+
     });
 
 })(window.jQuery);
